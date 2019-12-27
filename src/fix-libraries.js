6a2f7f0f590e1974f795f4849ff9c47fafa1672d
micabe <mickael@qare.io>
2019-12-27 17:45:45 +0100

    fix path for monorepo

--- src/fix-libraries.js
+++ src/fix-libraries.js
@@ -1,15 +1,19 @@
-const chalk = require('chalk');
-const path = require('path');
+const chalk = require("chalk");
+const path = require("path");
 
-const utilities = require('./utilities');
+const utilities = require("./utilities");
 
-function configListForConfig (configLists, configUuid) {
+function configListForConfig(configLists, configUuid) {
 	for (const listUuid of Object.keys(configLists)) {
-		if (listUuid.endsWith('_comment')) continue;
+		if (listUuid.endsWith("_comment")) continue;
 
 		const configList = configLists[listUuid];
 
-		if (configList.buildConfigurations.find(config => config.value === configUuid)) {
+		if (
+			configList.buildConfigurations.find(
+				config => config.value === configUuid
+			)
+		) {
 			return configList;
 		}
 	}
@@ -17,34 +21,40 @@ function configListForConfig (configLists, configUuid) {
 	return null;
 }
 
-function updateProject (project) {
-
+function updateProject(project) {
 	const configs = project.pbxXCBuildConfigurationSection();
 	const configLists = project.pbxXCConfigurationList();
 	let changed = false;
 	const { Debug, Release } = utilities.getMappings();
 	const mappings = {
 		Debug,
-		Release,
+		Release
 	};
 
 	// Go through each mapping in our debug map and figure out if we need to clone it.
 	for (const sourceBuildConfig of Object.keys(mappings)) {
 		for (const destinationBuildConfig of mappings[sourceBuildConfig]) {
 			// Do we have the clone already?
-			const buildConfig = project.getBuildConfigByName(destinationBuildConfig);
+			const buildConfig = project.getBuildConfigByName(
+				destinationBuildConfig
+			);
 
 			// If we get an empty object back, then it's not able to find the build config.
 			if (Object.keys(buildConfig).length === 0) {
 				// Ok, we need to create our clone of the build configs they've asked for and add it to the config lists.
-				const sourceConfigs = project.getBuildConfigByName(sourceBuildConfig);
+				const sourceConfigs = project.getBuildConfigByName(
+					sourceBuildConfig
+				);
 
 				// There are actually multiple of the same configs spread across multiple lists. Clone them all to the destination build configs.
 				for (const key of Object.keys(sourceConfigs)) {
 					const sourceConfig = sourceConfigs[key];
 					const configList = configListForConfig(configLists, key);
 
-					if (!configList) throw new Error(`Unable to find config list for build configuration: ${sourceConfig.name}`);
+					if (!configList)
+						throw new Error(
+							`Unable to find config list for build configuration: ${sourceConfig.name}`
+						);
 
 					// Copy that bad boy.
 					const clone = JSON.parse(JSON.stringify(sourceConfig));
@@ -55,14 +65,34 @@ function updateProject (project) {
 
 					configs[configurationUuid] = clone;
 					configs[configurationCommentKey] = destinationBuildConfig;
-					configList.buildConfigurations.push({ value: configurationUuid, comment: destinationBuildConfig });
+					configList.buildConfigurations.push({
+						value: configurationUuid,
+						comment: destinationBuildConfig
+					});
 				}
 
-				console.log(chalk.gray(` ${chalk.green('✔')} [fix-libraries]: ${chalk.green(sourceBuildConfig + ' -> ' + destinationBuildConfig + ' created')} in ${path.dirname(path.relative(process.cwd(), project.filepath))}`));
+				console.log(
+					chalk.gray(
+						` ${chalk.green("✔")} [fix-libraries]: ${chalk.green(
+							sourceBuildConfig +
+								" -> " +
+								destinationBuildConfig +
+								" created"
+						)} in ${path.dirname(
+							path.relative(process.cwd(), project.filepath)
+						)}`
+					)
+				);
 
 				changed = true;
 			} else {
-				console.log(chalk.gray(` - [fix-libraries]: ${sourceBuildConfig} -> ${destinationBuildConfig} skipped in ${path.dirname(path.relative(process.cwd(), project.filepath))}`));
+				console.log(
+					chalk.gray(
+						` - [fix-libraries]: ${sourceBuildConfig} -> ${destinationBuildConfig} skipped in ${path.dirname(
+							path.relative(process.cwd(), project.filepath)
+						)}`
+					)
+				);
 			}
 		}
 	}
@@ -70,12 +100,15 @@ function updateProject (project) {
 	return changed;
 }
 
-module.exports = function findAndFix (singleProject = null) {
+module.exports = function findAndFix(singleProject = null) {
 	// Find all of the pbxproj files we care about.
-	let pattern = './node_modules/**/*.xcodeproj/project.pbxproj';
+	let pattern = "../../node_modules/**/*.xcodeproj/project.pbxproj";
 	// Search only for specified project
 	if (singleProject) {
-		pattern = './node_modules/**/' + singleProject + '.xcodeproj/project.pbxproj';
+		pattern =
+			"../../node_modules/**/" +
+			singleProject +
+			".xcodeproj/project.pbxproj";
 	}
 
 	utilities.updateProjectsMatchingGlob(pattern, (err, project) => {
--- src/fix-script.js
+++ src/fix-script.js
@@ -1,9 +1,9 @@
-const chalk = require('chalk');
-const path = require('path');
+const chalk = require("chalk");
+const path = require("path");
 
-const utilities = require('./utilities');
+const utilities = require("./utilities");
 
-function updateProject (project) {
+function updateProject(project) {
 	console.log(path.dirname(path.relative(process.cwd(), project.filepath)));
 
 	const section = project.hash.project.objects.PBXShellScriptBuildPhase;
@@ -12,50 +12,68 @@ function updateProject (project) {
 		// Look for the React native script.
 		const step = section[key];
 
-		if (step && step.shellScript && step.shellScript.indexOf('react-native-xcode.sh') >= 0) {
+		if (
+			step &&
+			step.shellScript &&
+			step.shellScript.indexOf("react-native-xcode.sh") >= 0
+		) {
 			// Found it!
 			// Need to add our actual mappings to the project.
 			const mappings = utilities.getMappings();
-			const configurations = (mappings.Debug || []).join('|');
-			const scriptSettings = (mappings.settings && mappings.settings['fix-script']) || {};
-			const nodeCommand = scriptSettings.nodeCommand ? scriptSettings.nodeCommand + ' ' : '';
+			const configurations = (mappings.Debug || []).join("|");
+			const scriptSettings =
+				(mappings.settings && mappings.settings["fix-script"]) || {};
+			const nodeCommand = scriptSettings.nodeCommand
+				? scriptSettings.nodeCommand + " "
+				: "";
 			const env = scriptSettings.env || [];
 
-			const devConfigs = `\\"+(${configurations}${configurations.length ? '|' : ''}Debug)\\"`;
+			const devConfigs = `\\"+(${configurations}${
+				configurations.length ? "|" : ""
+			}Debug)\\"`;
 			env.DEVELOPMENT_BUILD_CONFIGURATIONS = devConfigs;
-			env.NODE_BINARY = env.NODE_BINARY || 'node';
+			env.NODE_BINARY = env.NODE_BINARY || "node";
 
 			const exports = Object.keys(env)
-				.map((key) => [key, env[key]])
+				.map(key => [key, env[key]])
 				.map(([key, value]) => `export ${key}=${value}`);
 
-			const runCommand = `${nodeCommand}../node_modules/react-native-schemes-manager/lib/react-native-xcode.sh`;
+			const runCommand = `${nodeCommand}../../../node_modules/react-native-schemes-manager/lib/react-native-xcode.sh`;
 
-			const commands = [
-				...exports,
-				runCommand,
-			];
+			const commands = [...exports, runCommand];
 
-			const newScript = `"${commands.join('\\n')}"`;
+			const newScript = `"${commands.join("\\n")}"`;
 
 			if (step.shellScript === newScript) {
 				// It's already up to date.
-				console.log(chalk.gray(` - [fix-script]: ${path.dirname(path.relative(process.cwd(), project.filepath))} skipped`));
+				console.log(
+					chalk.gray(
+						` - [fix-script]: ${path.dirname(
+							path.relative(process.cwd(), project.filepath)
+						)} skipped`
+					)
+				);
 				return false;
 			} else {
 				step.shellScript = newScript;
 
-				console.log(chalk.gray(` ${chalk.green('✔')} [fix-script]: ${path.dirname(path.relative(process.cwd(), project.filepath))} ${chalk.green('fixed')}`));
+				console.log(
+					chalk.gray(
+						` ${chalk.green("✔")} [fix-script]: ${path.dirname(
+							path.relative(process.cwd(), project.filepath)
+						)} ${chalk.green("fixed")}`
+					)
+				);
 				return true;
 			}
 		}
 	}
 }
 
-module.exports = function findAndFix () {
+module.exports = function findAndFix() {
 	let projectDirectory = utilities.getMappings().projectDirectory;
 	if (!projectDirectory) {
-		projectDirectory = 'ios';
+		projectDirectory = "ios";
 	}
 
 	// Find all of the pbxproj files we care about.
